692c8e1909a9d9f642d8595310a002c45edae1ba
dbecker92691 <dbecker92691@gmail.com>
2019-04-18 13:15:44 -0600

    created handle login/register in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,98 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Login from "./Login/Login";
+import Main from "./Main/Main";
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      currentUser: {
+        "username": ""
+      },
+      loggedIn: false,
+      error: ""
+    }
+  }
+
+  handleLogin = async (formData) => {
+    console.log(formData, "<---- login form data");
+    console.log(JSON.stringify(formData), "<----- stringified login form data");
+
+    const newLoginUser = await fetch(`${process.env.REACT_APP_BACKEND_ADDRESS_LOGIN}/login`, {
+      method: "POST",
+      credentials: "include",
+      body: JSON.stringify(formData),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    });
+
+    console.log(newLoginUser, "<--- login fetch request");
+
+    if(newLoginUser.status === 200){
+      const newUserResponse = await newLoginUser.json();
+
+      this.setState({
+        currentUser: newUserResponse,
+        loggedIn: true,
+        error: ""
+      });
+
+      console.log("successful login");
+    }else{
+      this.setState({
+        error: "Invalid login credentails"
+      });
+
+      console.log(this.state.error);
+    }
+  }
+
+  handleRegister = async (formData) => {
+
+    console.log(formData, "<----- register form data");
+    console.log(JSON.stringify(formData), "<----- strigified regiester form data");
+
+    const newRegisterUser = await fetch(`${process.env.REACT_APP_BACKEND_ADDRESS_LOGIN}/register`, {
+      method: "POST",
+      credentails: "include",
+      body: JSON.stringify(formData),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    });
+
+    console.log(newRegisterUser, "<---- register fetch request");
+
+    const newRegisterUserResponse = await newRegisterUser.json();
+
+    if(newRegisterUser.status === 200){
+      this.setState({
+        currentUser: newRegisterUserResponse,
+        loggedIn: true,
+        error: ""
+      })
+      console.log("successful register");
+    }else{
+      this.setState({
+        error: "Invalid register"
+      })
+
+      console.log(this.state.error);
+    }
+
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        {
+          this.state.loggedIn ?
+          <Main /> :
+          <Login handleLogin={this.handleLogin} handleRegister={this.handleRegister} />
+
+        }
       </div>
     );
   }

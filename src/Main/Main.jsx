3b675b6e448412ff88d3837147ef473da296742c
dbecker92691 <dbecker92691@gmail.com>
2019-04-24 10:29:29 -0600

    added nav bar to main component

--- src/Main/Main.jsx
+++ src/Main/Main.jsx
@@ -1,6 +1,5 @@
 import React, {Component} from 'react';
-
-
+import NavBar from "./NavBar"
 
 
 
@@ -8,6 +7,7 @@ export default class Main extends Component {
 	render(){
 		return(
 			<div>
+			<NavBar />
 				Here is my main component!!!!!!
 			</div>
 		)
